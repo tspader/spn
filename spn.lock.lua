9872f8eb51fcab139cb2c4823c5e39c16fa2141a
Thomas Spader <thomas.spader@gmail.com>
2025-10-20 18:14:17 -0400

    chore: bump lockfile

--- spn.lock.lua
+++ spn.lock.lua
@@ -1,22 +1,22 @@
 return {
   deps = {
     argparse = {
-      build_id = "5f95e5a6",
+      build_id = "88cbe228",
       commit = "45e0e563c4",
       name = "argparse"
     },
     luajit = {
-      build_id = "8bd8df4e",
-      commit = "871db2c84e",
+      build_id = "55b16ec2",
+      commit = "25a61a1821",
       name = "luajit"
     },
     sdl3 = {
-      build_id = "458cd9f8",
-      commit = "5e1a753f88",
+      build_id = "c7d8c382",
+      commit = "91a5598283",
       name = "sdl3"
     },
     sp = {
-      build_id = "915da721",
+      build_id = "e98cf5fe",
       commit = "2810c48808",
       name = "sp"
     }
--- test/spn.lock.lua
+++ test/spn.lock.lua
@@ -1,12 +1,12 @@
 return {
   deps = {
     argparse = {
-      build_id = "5f95e5a6",
+      build_id = "88cbe228",
       commit = "45e0e563c4",
       name = "argparse"
     },
     sp = {
-      build_id = "915da721",
+      build_id = "e98cf5fe",
       commit = "2810c48808",
       name = "sp"
     }
