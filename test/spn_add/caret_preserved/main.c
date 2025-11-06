91b5d3febf83da8033abafa1e3868ed676eb45d8
Thomas Spader <thomas.spader@gmail.com>
2025-11-06 01:09:30 -0500

    test

---
+++ test/spn_add/caret_preserved/main.c
@@ -0,0 +1,11 @@
+// spn add argparse
+// spn build
+// verify sp still has a ^ in the project file
+
+#define SP_IMPLEMENTATION
+#include "sp.h"
+
+s32 main(s32 num_args, const c8** args) {
+  SP_LOG("hello, {:fg brightcyan}", SP_FMT_CSTR("world"));
+  SP_EXIT_SUCCESS();
+}
