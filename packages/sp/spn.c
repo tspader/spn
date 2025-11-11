c4900300adc6f920cffb6ae092070def3c1ed638
Thomas Spader <thomas.spader@gmail.com>
2025-11-11 18:21:32 -0500

    chore: package metadata

--- packages/sp/spn.c
+++ packages/sp/spn.c
@@ -1,8 +1,5 @@
 #include "spn.h"
-
-void build(spn_dep_context_t* dep) {
-  spn_dep_log(dep, "hello, world\n");
-}
+#include "stdio.h"
 
 void package(spn_dep_context_t* dep) {
   spn_copy(dep, SPN_DIR_SOURCE, "sp.h", SPN_DIR_INCLUDE, "");
