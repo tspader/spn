b4227553e8db7a43b53d333f2fb83f09c377236d
Thomas Spader <thomas.spader@gmail.com>
2026-01-06 01:58:59 -0500

    test: fix

--- test/manual/spn_build/add_define/spn.c
+++ test/manual/spn_build/add_define/spn.c
@@ -1,8 +1,7 @@
 #include "spn.h"
 
 void configure(spn_build_ctx_t* b) {
-  spn_pkg_t* pkg = spn_get_pkg(b);
-  spn_pkg_add_define(pkg, "PKG_DEFINE");
+  spn_add_define(b, "PKG_DEFINE");
 
   spn_target_t* target = spn_get_target(b, "main");
   spn_target_add_define(target, "TARGET_DEFINE");
--- test/manual/spn_build/add_system_dep/spn.c
+++ test/manual/spn_build/add_system_dep/spn.c
@@ -1,6 +1,5 @@
 #include "spn.h"
 
 void configure(spn_build_ctx_t* b) {
-  spn_pkg_t* pkg = spn_get_pkg(b);
-  spn_pkg_add_system_dep(pkg, "m");
+  spn_add_system_dep(b, "m");
 }
