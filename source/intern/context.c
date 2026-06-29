ca180e6eca152fc0292ad060e9ff2e5d4fdb6199
Thomas Spader <thomas.spader@gmail.com>
2026-06-28 23:36:34 -0400

    build script stuff

--- source/intern/context.c
+++ source/intern/context.c
@@ -1,18 +1,3 @@
 #include "intern/intern.h"
 #include "ctx/ctx.h"
 
-sp_str_t spn_intern(sp_str_t str) {
-  return sp_intern_get_or_insert_str(spn_ctx_get_intern(), str);
-}
-
-sp_str_t spn_intern_cstr(const c8* cstr) {
-  return sp_intern_get_or_insert_str(spn_ctx_get_intern(), sp_str_view(cstr));
-}
-
-bool spn_intern_is_equal(sp_str_t a, sp_str_t b) {
-  return sp_intern_is_equal_str(spn_ctx_get_intern(), a, b);
-}
-
-bool spn_intern_is_equal_cstr(sp_str_t str, const c8* cstr) {
-  return sp_intern_is_equal_str(spn_ctx_get_intern(), str, sp_str_view(cstr));
-}
