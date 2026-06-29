b44d314c20b03c8a0f390a8118d4f00e7fccd938
Thomas Spader <thomas.spader@gmail.com>
2026-06-28 23:36:34 -0400

    build script stuff

--- spn.c
+++ spn.c
@@ -6,5 +6,6 @@ spn_err_t configure(spn_t* spn, spn_config_t* config) {
   const c8* dir = spn_get_subdir(tcc, SPN_DIR_LIB, "tcc");
   spn_target_embed_dir_ex(target, dir, "u8", "u64");
   spn_target_embed_file_ex(target, "include/spn.h", "include_spn_h", "u8", "u64");
+  spn_target_embed_file_ex(target, "source/toolchain/toolchains.json", "toolchains_json", "u8", "u64");
   return SPN_OK;
 }
