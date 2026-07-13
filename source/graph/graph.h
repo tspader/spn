fdbe079b6d3ac84d187510ebc44a57cf307c3c94
Thomas Spader <thomas.spader@gmail.com>
2026-07-13 00:46:51 -0400

    cas

--- source/graph/graph.h
+++ source/graph/graph.h
@@ -1,6 +1,105 @@
 #ifndef SPN_GRAPH_GRAPH_H
 #define SPN_GRAPH_GRAPH_H
 
+#include "unit/types.h"
 #include "sp/sp_graph.h"
+
+SP_TYPEDEF_FN(s32, spn_bg_fn_t, spn_bg_cmd_t* cmd, void* user_data);
+SP_TYPEDEF_FN(u64, spn_bg_hash_fn_t, void*, u64);
+
+typedef u64 spn_dag_hash_t;
+
+typedef struct {
+  u32 index;
+  u32 occupied;
+} spn_dag_id_t;
+
+typedef enum {
+  SPN_DAG_ARTIFACT_KIND_VALUE,
+  SPN_DAG_ARTIFACT_KIND_FILE,
+} spn_dag_artifact_kind_t;
+
+typedef struct {
+  spn_dag_id_t id;
+  spn_dag_artifact_kind_t kind;
+  spn_dag_id_t producer;
+  sp_da(spn_dag_id_t) consumers;
+} spn_dag_artifact_t;
+
+typedef struct {
+  spn_dag_id_t id;
+  spn_bg_fn_t execute;
+  spn_bg_hash_fn_t hash;
+  void* user_data;
+  sp_da(spn_dag_id_t) consumes;
+  sp_da(spn_dag_id_t) produces;
+} spn_dag_action_t;
+
+typedef struct {
+  spn_bg_fn_t execute;
+  spn_bg_hash_fn_t hash;
+  void* user_data;
+} spn_dag_action_config_t;
+
+typedef struct {
+  sp_mem_arena_t* arena;
+  sp_mem_t mem;
+  sp_da(spn_dag_artifact_t) artifacts;
+  sp_da(spn_dag_action_t) actions;
+} spn_dag_t;
+
+spn_dag_t* spn_dag_new(sp_mem_t mem) {
+  spn_dag_t* g = sp_alloc_type(mem, spn_dag_t);
+  g->arena = sp_mem_arena_new(mem);
+  g->mem = sp_mem_arena_as_allocator(g->arena);
+  g->artifacts = sp_da_new(g->mem, spn_dag_artifact_t);
+  g->actions = sp_da_new(g->mem, spn_dag_action_t);
+  return g;
+}
+
+spn_dag_id_t spn_dag_add_value(spn_dag_t* g, spn_dag_hash_t hash) {
+  spn_dag_artifact_t artifact = {
+    .id = {
+      .index = (u32)sp_da_size(g->artifacts),
+      .occupied = true
+    },
+  };
+  sp_da_push(g->artifacts, artifact);
+  return artifact.id;
+}
+
+spn_dag_id_t spn_dag_add_action(spn_dag_t* g, spn_dag_action_config_t action) {
+  return sp_zero_s(spn_dag_id_t);
+}
+typedef struct {
+  spn_dag_id_t toolchain;
+  spn_dag_id_t os;
+  spn_dag_id_t arch;
+  spn_dag_id_t abi;
+  spn_dag_id_t linkage;
+  spn_dag_id_t standard;
+  spn_dag_id_t mode;
+  spn_dag_id_t opt;
+  spn_dag_id_t sanitizers;
+} spn_build_nodes_t;
+
+void add_build(sp_mem_t mem, spn_build_unit_t* build) {
+  spn_dag_t* g = spn_dag_new(mem);
+
+  spn_build_nodes_t nodes = sp_zero;
+  nodes.toolchain = spn_dag_add_value(g, build->toolchain->identity);
+  nodes.os = spn_dag_add_value(g, build->profile.os);
+  nodes.arch = spn_dag_add_value(g, build->profile.arch);
+  nodes.abi = spn_dag_add_value(g, build->profile.abi);
+  nodes.linkage = spn_dag_add_value(g, build->profile.linkage);
+  nodes.standard = spn_dag_add_value(g, build->profile.standard);
+  nodes.mode = spn_dag_add_value(g, build->profile.mode);
+  nodes.opt = spn_dag_add_value(g, build->profile.opt);
+  nodes.sanitizers = spn_dag_add_value(g, build->profile.sanitizers);
+}
+
+void add_target(sp_mem_t mem, spn_dag_t* g, spn_build_nodes_t* n) {
+
+}
 
 #endif
