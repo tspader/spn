f2dd83a929e606e73203d06f895fab9f9b6d9b5a
Thomas Spader <thomas.spader@gmail.com>
2026-07-13 01:19:49 -0400

    more cas

--- source/toolchain/sha256.h
+++ source/toolchain/sha256.h
@@ -4,8 +4,18 @@
 #include "sp.h"
 #include "spn.h"
 
+typedef struct {
+  u32 state [8];
+  u64 length;
+  u8 block [64];
+  u32 fill;
+} spn_sha256_ctx_t;
+
 void      spn_sha256(const void* data, u64 len, u8 digest[32]);
 sp_str_t  spn_sha256_hex(sp_mem_t mem, const void* data, u64 len);
 spn_err_t spn_sha256_file(sp_mem_t mem, sp_str_t path, sp_str_t* hex);
+void      spn_sha256_init(spn_sha256_ctx_t* ctx);
+void      spn_sha256_update(spn_sha256_ctx_t* ctx, const u8* data, u64 len);
+void      spn_sha256_final(spn_sha256_ctx_t* ctx, u8 digest [32]);
 
 #endif
