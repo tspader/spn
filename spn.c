22a96927bef47dec444cbf374acbf90a514c2852
Thomas Spader <thomas.spader@gmail.com>
2026-07-15 01:26:59 -0400

    chore: nuke old build script

--- spn.c
+++
@@ -1,8 +0,0 @@
-#include "spn.h"
-
-spn_err_t configure(spn_t* spn, spn_config_t* config) {
-  spn_target_t* target = spn_get_target(spn, "spn");
-  spn_target_embed_file_ex(target, "include/spn.h", "include_spn_h", "u8", "u64");
-  spn_target_embed_file_ex(target, "source/toolchain/toolchains.json", "toolchains_json", "u8", "u64");
-  return SPN_OK;
-}
