d0bc1d74934e36a68906d133c3a362334d6f6545
Thomas Spader <thomas.spader@gmail.com>
2026-07-15 01:13:02 -0400

    dep: bump sp.h

--- tools/cmake/fetch.cmake
+++ tools/cmake/fetch.cmake
@@ -1,7 +1,7 @@
 cmake_minimum_required(VERSION 3.24)
 
 set(DEPS
-  "sp|https://github.com/tspader/sp.git|f58f6e6e3ef79ebc52b635d90ab09b18fde2249c"
+  "sp|https://github.com/tspader/sp.git|cb474f197d44a332e3a7e6fd84dff06e276f7d4a"
   "toml|https://github.com/tspader/toml.git|2e8ffdfa215cfe184fd06f8f48b47fbd04a4a678"
   "argparse|https://github.com/tspader/argparse.git|f71ed6c7b11cdbe75ffa0b42170530cc8610cbbf"
   "yyjson|https://github.com/ibireme/yyjson.git|ad58f21bee1213a8fdd614c2a11b4453815a73e9"
