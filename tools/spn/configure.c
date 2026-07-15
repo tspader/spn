de6275e16abbdc38d48da4fcb3699d95f25336d0
Thomas Spader <thomas.spader@gmail.com>
2026-07-15 01:26:23 -0400

    build: embed init stuff

--- tools/spn/configure.c
+++ tools/spn/configure.c
@@ -12,5 +12,6 @@ spn_err_t configure(spn_t* spn, spn_config_t* config) {
   spn_target_t* target = spn_get_target(spn, "spn");
   spn_target_embed_file_ex(target, "include/spn.h", "include_spn_h", "u8", "u64");
   spn_target_embed_file_ex(target, "source/toolchain/toolchains.json", "toolchains_json", "u8", "u64");
+  spn_target_embed_dir_ex(target, "assets/init", "init", "u8", "u64");
   return SPN_OK;
 }
