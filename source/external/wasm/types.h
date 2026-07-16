ef30f4022a9adc69a03a362fe64640b9247f5a4e
Thomas Spader <thomas.spader@gmail.com>
2026-07-16 03:41:45 -0400

    wasm: hook observations into package script

--- source/external/wasm/types.h
+++ source/external/wasm/types.h
@@ -1,12 +1,20 @@
 #ifndef SPN_WASM_TYPES_H
 #define SPN_WASM_TYPES_H
 
+#include "dag/types.h"
 #include "forward/types.h"
 #include "spn.h"
 
 typedef struct WASMModuleCommon spn_wasm_module_t;
 typedef struct WASMModuleInstanceCommon spn_wasm_instance_t;
+typedef struct WASMExecEnv spn_wasm_exec_env_t;
 typedef struct spn_wasm_handles_t spn_wasm_handles_t;
+typedef struct spn_dag_wasi_t spn_dag_wasi_t;
+
+typedef struct {
+  sp_mem_t mem;
+  sp_da(spn_dag_obs_t)* out;
+} spn_wasm_obs_t;
 
 typedef union {
   struct {
@@ -31,7 +39,9 @@ typedef struct {
   sp_mutex_t mutex;
   spn_wasm_module_t* module;
   spn_wasm_instance_t* instance;
+  spn_wasm_exec_env_t* env;
   spn_wasm_handles_t* handles;
+  spn_dag_wasi_t* wasi;
   u32 ctx;
   spn_wasm_preopens_t preopens;
 } spn_wasm_script_t;
