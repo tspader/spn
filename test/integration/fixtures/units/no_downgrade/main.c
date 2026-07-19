96cd7b7421352003e32d6e392a4add82a50adb4c
Thomas Spader <thomas.spader@gmail.com>
2026-07-19 00:02:22 -0400

    test: cleanup

--- test/integration/fixtures/units/no_downgrade/main.c
+++ test/integration/fixtures/units/no_downgrade/main.c
@@ -1,5 +1,9 @@
 #include "foo.h"
 
+#if FOO_VERSION != 19
+#error "expected foo 1.9.0"
+#endif
+
 int main() {
-  return foo_version() == 19 ? 0 : 1;
+  return foo_version() == FOO_VERSION ? 0 : 1;
 }
