4c9cc1ea2378b1278212372bab742471bf4d4b56
Thomas Spader <thomas.spader@gmail.com>
2026-07-29 03:01:13 -0400

    more tui bullshit; not permanent

--- source/spn.c
+++ source/spn.c
@@ -188,6 +188,12 @@ sp_app_result_t spn_init(sp_app_t* sp) {
   sp_io_stream_writer_from_fd(&spn.logger.err, sp_sys_stderr, SP_IO_CLOSE_MODE_NONE);
   if (sp_sys_is_tty(sp_sys_stdout)) sp_sys_tty_use_vt(sp_sys_stdout);
   if (sp_sys_is_tty(sp_sys_stderr)) sp_sys_tty_use_vt(sp_sys_stderr);
+#ifdef SP_WIN32
+  if (sp_sys_is_tty(sp_sys_stdout) || sp_sys_is_tty(sp_sys_stderr)) {
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+  }
+#endif
   spn.logger.level = SPN_LOG_LEVEL_INFO;
   sp_str_t log_level = sp_env_get(spn.env, sp_str_lit("SPN_LOG_LEVEL"));
   if (!sp_str_empty(log_level)) {
