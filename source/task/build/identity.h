ca76e03cee76db4675e6db6d61c79216f7723676
Thomas Spader <thomas.spader@gmail.com>
2026-07-28 22:07:11 -0400

    dag: fix git checkout identity

--- source/task/build/identity.h
+++ source/task/build/identity.h
@@ -5,10 +5,18 @@
 #include "pkg/types.h"
 #include "unit/types.h"
 
-bool             spn_build_copy_to_include(spn_publish_copy_t* copy, sp_str_t* rest);
-bool             spn_build_path_within(sp_str_t path, sp_str_t dir);
-spn_dag_digest_t spn_build_tree_identity(const spn_dag_roots_t* roots, spn_pkg_unit_t* unit);
-spn_dag_digest_t spn_build_package_identity(spn_pkg_unit_t* unit);
-spn_dag_digest_t spn_build_user_identity(const spn_dag_roots_t* roots, spn_user_node_t* node);
+typedef struct {
+  spn_pkg_tree_kind_t kind;
+  sp_str_t rev;
+  sp_str_t dir;
+  sp_hash_t patches;
+} spn_build_source_pin_t;
+
+bool                   spn_build_copy_to_include(spn_publish_copy_t* copy, sp_str_t* rest);
+bool                   spn_build_path_within(sp_str_t path, sp_str_t dir);
+spn_build_source_pin_t spn_build_source_pin(spn_pkg_unit_t* unit);
+spn_dag_digest_t       spn_build_tree_identity(const spn_dag_roots_t* roots, spn_pkg_unit_t* unit, const spn_build_source_pin_t* pin);
+spn_dag_digest_t       spn_build_package_identity(spn_pkg_unit_t* unit, const spn_build_source_pin_t* pin);
+spn_dag_digest_t       spn_build_user_identity(const spn_dag_roots_t* roots, spn_user_node_t* node, const spn_build_source_pin_t* pin);
 
 #endif
