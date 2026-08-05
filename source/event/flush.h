54639e99a4aec3333bb8aa31011c1af4da7cf307
Thomas Spader <thomas.spader@gmail.com>
2026-08-05 02:10:33 -0400

    write unit store logs from the library, not the shell

---
+++ source/event/flush.h
@@ -0,0 +1,8 @@
+#ifndef SPN_EVENT_FLUSH_H
+#define SPN_EVENT_FLUSH_H
+
+#include "event/types.h"
+
+sp_da(spn_build_event_t) spn_event_flush(sp_mem_t mem, spn_event_buffer_t* events);
+
+#endif
--- source/shell/shell.c
+++ source/shell/shell.c
@@ -1,8 +1,7 @@
 #include "shell/shell.h"
 
 #include "ctx/types.h"
-#include "event/build.h"
-#include "event/event.h"
+#include "event/flush.h"
 #include "event/log.h"
 #include "sp/io.h"
 #include "tui/tui.h"
@@ -96,7 +95,7 @@ void spn_shell_flush() {
   if (!spn.events) return;
 
   sp_mem_arena_marker_t s = sp_mem_begin_scratch();
-  sp_da(spn_build_event_t) events = spn_event_buffer_drain(s.mem, spn.events);
+  sp_da(spn_build_event_t) events = spn_event_flush(s.mem, spn.events);
 
   sp_da_for(events, it) {
     spn_build_event_t* event = &events[it];
@@ -104,10 +103,6 @@ void spn_shell_flush() {
 
     if (shell.logger.jsonl.fd) {
       spn_event_log_jsonl(&shell.logger.jsonl.base, event);
-    }
-    if (event->io) {
-      spn_event_log_jsonl(&event->io->jsonl.writer, event);
-      spn_event_log_build(&event->io->build.writer, event);
     }
 
     spn_tui_log_event(&shell.tui, event);
