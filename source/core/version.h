bff645ac0ec826d21b49d6b7ee96224470630326
Thomas Spader <thomas.spader@gmail.com>
2026-08-22 01:37:50 -0400

    build: stamp version, channel, and commit into the binary

--- source/core/version.h
+++ source/core/version.h
@@ -1,7 +1,22 @@
 #ifndef SPN_VERSION_H
 #define SPN_VERSION_H
 
-#define SPN_VERSION "1.0.0"
-#define SPN_COMMIT "00c0fa98"
+#if defined(__has_include)
+  #if __has_include("build_info.gen.h")
+    #include "build_info.gen.h"
+  #endif
+#endif
+
+#ifndef SPN_VERSION
+  #define SPN_VERSION "0.2.0"
+#endif
+
+#ifndef SPN_BUILD_CHANNEL
+  #define SPN_BUILD_CHANNEL "dev"
+#endif
+
+#ifndef SPN_BUILD_COMMIT
+  #define SPN_BUILD_COMMIT ""
+#endif
 
 #endif
