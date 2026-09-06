851961792d146e2161ead39dd2ee236b6221eb35
Thomas Spader <thomas.spader@gmail.com>
2026-09-06 16:00:56 -0400

    smoke: rename the fixtures

--- test/smoke/elf.c
+++ test/smoke/elf.c
@@ -4,7 +4,7 @@ sp_test(elf, gnu_script) {
   return run_test(t, (test_t) {
     .project = "test/smoke/fixtures/elf_gnu_script",
     .copy = { "main.ld" },
-    .when = { .toolchain = "G", .host = SPN_OS_LINUX, .programs = { "gcc", "ar" } },
+    .when = { .toolchain = "gnu", .host = SPN_OS_LINUX, .programs = { "gcc", "ar" } },
     .actions = {
       { .kind = ACTION_RUN_CLI, .cli = { .cmd = "build", .args = { "--target", SPN_TEST_ARCH "-freestanding" } } },
       { .kind = ACTION_VERIFY_ELF_ENTRY, .verify_elf_entry = { target_exe("main", SPN_TEST_ARCH "-freestanding-none"), 0x400000 } },
@@ -16,7 +16,7 @@ sp_test(elf, lld_script) {
   return run_test(t, (test_t) {
     .project = "test/smoke/fixtures/elf_lld_script",
     .copy = { "main.ld" },
-    .when = { .toolchain = "GL", .host = SPN_OS_LINUX, .programs = { "gcc", "ar", "ld.lld" } },
+    .when = { .toolchain = "gcc-lld", .host = SPN_OS_LINUX, .programs = { "gcc", "ar", "ld.lld" } },
     .actions = {
       { .kind = ACTION_RUN_CLI, .cli = { .cmd = "build", .args = { "--target", SPN_TEST_ARCH "-freestanding" } } },
       { .kind = ACTION_VERIFY_ELF_ENTRY, .verify_elf_entry = { target_exe("main", SPN_TEST_ARCH "-freestanding-none"), 0x400000 } },
@@ -27,7 +27,7 @@ sp_test(elf, lld_script) {
 sp_test(elf, clang_gnu) {
   return run_test(t, (test_t) {
     .project = "test/smoke/fixtures/elf_clang_gnu",
-    .when = { .toolchain = "C", .host = SPN_OS_LINUX, .programs = { "clang", "ld", "ar" } },
+    .when = { .toolchain = "clang-gnu", .host = SPN_OS_LINUX, .programs = { "clang", "ld", "ar" } },
     .actions = {
       { .kind = ACTION_RUN_CLI, .cli = { .cmd = "build" } },
       { .kind = ACTION_VERIFY_EVENT, .verify_event = { .event = SPN_EVENT_LINK_PASSED } },
@@ -38,7 +38,7 @@ sp_test(elf, clang_gnu) {
 sp_test(elf, clang_lld) {
   return run_test(t, (test_t) {
     .project = "test/smoke/fixtures/elf_clang_lld",
-    .when = { .toolchain = "LV", .host = SPN_OS_LINUX, .programs = { "clang", "ld.lld", "llvm-ar" } },
+    .when = { .toolchain = "clang-lld", .host = SPN_OS_LINUX, .programs = { "clang", "ld.lld", "llvm-ar" } },
     .actions = {
       { .kind = ACTION_RUN_CLI, .cli = { .cmd = "build" } },
       { .kind = ACTION_VERIFY_EVENT, .verify_event = { .event = SPN_EVENT_LINK_PASSED } },
@@ -49,7 +49,7 @@ sp_test(elf, clang_lld) {
 sp_test(elf, gcc_lld_hosted) {
   return run_test(t, (test_t) {
     .project = "test/smoke/fixtures/elf_gcc_lld_hosted",
-    .when = { .toolchain = "GLH", .host = SPN_OS_LINUX, .programs = { "gcc", "ar", "ld.lld" } },
+    .when = { .toolchain = "gcc-lld", .host = SPN_OS_LINUX, .programs = { "gcc", "ar", "ld.lld" } },
     .actions = {
       { .kind = ACTION_RUN_CLI, .cli = { .cmd = "build" } },
       { .kind = ACTION_VERIFY_EVENT, .verify_event = { .event = SPN_EVENT_LINK_PASSED } },
@@ -73,7 +73,7 @@ sp_test(elf, cross_gnu_script) {
   return run_test(t, (test_t) {
     .project = "test/smoke/fixtures/elf_cross_gnu_script",
     .copy = { "main.ld" },
-    .when = { .toolchain = "X", .programs = { "aarch64-linux-gnu-gcc", "aarch64-linux-gnu-ar" } },
+    .when = { .toolchain = "aarch64-gnu", .programs = { "aarch64-linux-gnu-gcc", "aarch64-linux-gnu-ar" } },
     .actions = {
       { .kind = ACTION_RUN_CLI, .cli = { .cmd = "build", .args = { "--target", "aarch64-freestanding" } } },
       { .kind = ACTION_VERIFY_ELF_ENTRY, .verify_elf_entry = { target_exe("main", "aarch64-freestanding-none"), 0x400000 } },
--- test/smoke/mingw.c
+++ test/smoke/mingw.c
@@ -4,7 +4,7 @@ sp_test(mingw, gnu_script) {
   return run_test(t, (test_t) {
     .project = "test/smoke/fixtures/mingw_gnu_script",
     .copy = { "main.ld" },
-    .when = { .toolchain = "M", .programs = { "x86_64-w64-mingw32-gcc", "x86_64-w64-mingw32-ar" } },
+    .when = { .toolchain = "mingw-gnu", .programs = { "x86_64-w64-mingw32-gcc", "x86_64-w64-mingw32-ar" } },
     .actions = {
       { .kind = ACTION_RUN_CLI, .cli = { .cmd = "build", .args = { "--target", "x86_64-windows-gnu" } } },
       { .kind = ACTION_VERIFY_EVENT, .verify_event = { .event = SPN_EVENT_LINK_PASSED } },
@@ -39,7 +39,7 @@ sp_test(mingw, zig_script_rejected) {
 sp_test(mingw, clang_gnu) {
   return run_test(t, (test_t) {
     .project = "test/smoke/fixtures/mingw_clang_gnu",
-    .when = { .toolchain = "W", .programs = { "clang", "x86_64-w64-mingw32-ld", "x86_64-w64-mingw32-ar" } },
+    .when = { .toolchain = "clang-mingw", .programs = { "clang", "x86_64-w64-mingw32-ld", "x86_64-w64-mingw32-ar" } },
     .actions = {
       { .kind = ACTION_RUN_CLI, .cli = { .cmd = "build", .args = { "--target", "x86_64-windows-gnu" } } },
       { .kind = ACTION_VERIFY_EVENT, .verify_event = { .event = SPN_EVENT_LINK_PASSED } },
